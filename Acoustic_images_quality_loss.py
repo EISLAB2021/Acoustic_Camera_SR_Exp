8f15aefd1c34554ae74c0234b70f81bcba82ff55
ZHOUMarineer <135226032+ZHOUMarineer@users.noreply.github.com>
2024-06-26 15:18:48 +0900

    Add files via upload

---
+++ Acoustic_images_quality_loss.py
@@ -0,0 +1,55 @@
+from PIL import Image
+import os
+
+
+def compress_image(input_path, output_path, quality=''):
+    """
+    Compresses a single acoustic camera image for quality loss testing.
+
+    Args:
+    - input_path (str): Path to the input acoustic camera image.
+    - output_path (str): Path to save the compressed acoustic camera image.
+    - quality (int): Compression quality (0-100).
+
+    Returns:
+    - bool: True if successful, False otherwise.
+    """
+    try:
+        img = Image.open(input_path)
+        img.save(output_path, quality=quality, optimize=True)
+        return True
+    except Exception as e:
+        print(f"Failed to compress acoustic camera image {input_path}: {e}")
+        return False
+
+
+def process_folder(input_folder, output_folder, quality=None):
+    """
+    Processes acoustic raw files to image format JPEG
+    Processes all JPEG format of acoustic images in a folder by compressing them.
+
+    Args:
+    - input_folder (str): Path to the folder containing input acoustic camera images.
+    - output_folder (str): Path to save the compressed acousticc images.
+    - quality (int): Compression quality of acoustic images  (0-100).
+    """
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    num_processed = 0
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".jpg") or filename.endswith(".jpeg"):
+            input_path = os.path.join(input_folder, filename)
+            output_path = os.path.join(output_folder, filename)
+            if compress_image(input_path, output_path, quality):
+                num_processed += 1
+
+    print(f"Processed {num_processed} images.")
+
+
+if __name__ == "__main__":
+    input_folder = " "  # Path to input acoustic cameras
+    output_folder = " "  # Path to save compressed acoustic images
+    quality = 20  # Compression quality of acoustic camera images, range (0-100)
+
+    process_folder(input_folder, output_folder, quality)
